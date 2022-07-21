d95b5a3e9b8b43842d5263fad5d2847b0bb1fdc2
mdsajalali <itzsajal.me@gmail.com>
2022-07-21 16:47:54 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+function navHamburger() {
+    document.querySelector('.nav-container ul').style.display = 'block';
+    document.querySelector('.nav-close').style.display = 'block';
+}
+
+function navClose() {
+    document.querySelector('.nav-close').style.display = 'none';
+    document.querySelector('.nav-container ul').style.display = 'none';
+}
+
